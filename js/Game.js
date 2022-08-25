4f684810343888b2532af4eb3f27588d6cd4f442
NINJ444 <98404317+NINJ444@users.noreply.github.com>
2022-08-25 10:44:44 -0300

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -6,7 +6,18 @@ class Game {
     form.display();
     player = new Player();
     playerCount = player.getCount();
+    carro1 = createSprite(width/2-50,height-100);
+    carro1.addImage("carro1",carro1img);
+    carro1.scale=0.07;
+    carro2 = createSprite(width/2+100,height-100);
+    carro2.addImage("carro2",carro2img);
+    carro2.scale=0.07;
+    carros=[carro1,carro2];
 
+  }
+  //jogo começou
+  play(){
+    drawSprites()
   }
 
   //pegar o estado do jogo do BD
@@ -16,6 +27,11 @@ class Game {
       gameState = data.val();
     })
   }
-
+  //atualizar o gameState no banco de dados
+  updateState(state){
+    database.ref("/").update({
+      gameState: state,
+    })
+  }
 
 }//classe
--- sketch.js
+++ sketch.js
@@ -5,9 +5,15 @@ var database;
 var form, player;
 var playerCount;
 var gameState;
+var carro1,carro2,carro1img,carro2img;
+var pistaimg;
+var carros;
 
 function preload() {
   backgroundImage = loadImage("./assets/planodefundo.png");
+  carro1img = loadImage("./assets/car1.png");
+  carro2img = loadImage("./assets/car2.png");
+  pistaimg = loadImage("./assets/PISTA.png");
 }
 
 function setup() {
@@ -21,6 +27,12 @@ function setup() {
 
 function draw() {
   background(backgroundImage);
+  if(playerCount == 2){
+    game.updateState(1)
+  }
+  if(gameState == 1){
+    game.play();
+  }
 }
 
 function windowResized() {
